7534b7c5c6419f1549c9bc63452a2fce2096298b
Nischith B <nischith.balaji@outlook.com>
2022-01-09 16:59:40 +0530

    Added BoardingPass feature

--- genie/lib/profile_page.dart
+++ genie/lib/profile_page.dart
@@ -95,98 +95,309 @@ class UserProfile extends StatelessWidget {
   }
 
   Widget getprofileUI(BuildContext context, Map data) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.35,
-          child: Stack(
-            children: [
-              Container(
-                  height: MediaQuery.of(context).size.height * 0.35 - 40 / 0.35,
-                  decoration: BoxDecoration(
-                      color: Colors.indigo[300],
-                      borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(40),
-                          bottomRight: Radius.circular(40)))),
-              Positioned(
-                  right: 0,
-                  bottom: 0,
-                  left: 0,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 25),
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            child: Stack(
+              children: [
+                Container(
+                    height:
+                        MediaQuery.of(context).size.height * 0.35 - 40 / 0.35,
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25),
-                        color: Colors.grey[200],
-                        boxShadow: [
-                          BoxShadow(
-                              offset: Offset(0, 10),
-                              blurRadius: 50,
-                              color: Colors.indigo.withOpacity(0.23))
-                        ]),
-                    margin: const EdgeInsets.symmetric(horizontal: 35),
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          minRadius: 40,
-                          backgroundColor: Colors.transparent,
-                          child: Icon(
-                            Icons.account_circle_rounded,
-                            size: 80,
-                            color: Colors.indigo[300],
+                        color: Colors.indigo[300],
+                        borderRadius: const BorderRadius.only(
+                            bottomLeft: Radius.circular(40),
+                            bottomRight: Radius.circular(40)))),
+                Positioned(
+                    right: 0,
+                    bottom: 0,
+                    left: 0,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 25, horizontal: 10),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(25),
+                          color: Colors.grey[200],
+                          boxShadow: [
+                            BoxShadow(
+                                offset: Offset(0, 10),
+                                blurRadius: 50,
+                                color: Colors.indigo.withOpacity(0.23))
+                          ]),
+                      margin: const EdgeInsets.symmetric(horizontal: 35),
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            minRadius: 40,
+                            backgroundColor: Colors.transparent,
+                            child: Icon(
+                              Icons.account_circle_rounded,
+                              size: 80,
+                              color: Colors.indigo[300],
+                            ),
                           ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            data['message']['name'],
+                            style: const TextStyle(
+                                color: Colors.grey,
+                                fontSize: 40,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: 1.1),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            data['message']['id'],
+                            style: TextStyle(
+                                color: Colors.grey[400],
+                                fontSize: 17,
+                                letterSpacing: 1.1),
+                          ),
+                        ],
+                      ),
+                    )),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          BialRewards(
+            points: data['message']['rewards'],
+          ),
+          Options(
+              label: 'My Orders',
+              iconData: Icons.shopping_bag_rounded,
+              onTapHandler: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const MyOrders()));
+              }),
+          Options(
+            label: 'Boarding Pass',
+            iconData: Icons.airplane_ticket_rounded,
+            onTapHandler: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return const AlertDialog(
+                      title: Text('Boarding Pass'),
+                      actions: [
+                        PassDialogBox(),
+                      ],
+                    );
+                  });
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PassDialogBox extends StatefulWidget {
+  const PassDialogBox({Key? key}) : super(key: key);
+
+  @override
+  _PassDialogBoxState createState() => _PassDialogBoxState();
+}
+
+class _PassDialogBoxState extends State<PassDialogBox> {
+  var flightNoController = TextEditingController();
+  var pnrController = TextEditingController();
+  bool isSubmitted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+        child: isSubmitted
+            ? BoardingPass(
+                flightNo: flightNoController.text,
+                pnr: pnrController.text,
+              )
+            : PassForm(
+                flightNoController: flightNoController,
+                pnrController: pnrController,
+                submitHandler: () {
+                  setState(() {
+                    isSubmitted = true;
+                  });
+                },
+              ));
+  }
+}
+
+class BoardingPass extends StatelessWidget {
+  const BoardingPass({Key? key, required this.flightNo, required this.pnr})
+      : super(key: key);
+  final String flightNo;
+  final String pnr;
+
+  Future<Map> fetchBoardingPassDetails(String flightNo, String pnr) async {
+    const secureStorage = FlutterSecureStorage();
+    var url = Uri.parse(
+        'https://bialapp.azurewebsites.net/api/boardingpass?code=HwPgnocZeyUvcnXpLJ6a2T8TraVdqxCqGTZ1BeNvCTJAuMaEQ1kIwQ%3D%3D');
+    var body = {
+      'token': await secureStorage.read(key: 'signInToken'),
+      'flightNo': flightNo,
+      'PNR': pnr,
+    };
+    try {
+      var response = await http.post(url, body: json.encode(body));
+      return json.decode(response.body);
+    } on Exception {
+      return {
+        'message': {'name': '', 'seat': '', 'class': ''}
+      };
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+                offset: Offset(0, 10),
+                blurRadius: 50,
+                color: Colors.indigo.withOpacity(0.33))
+          ],
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      // height: MediaQuery.of(context).size.height * 0.4,
+      // width: MediaQuery.of(context).size.width * 0.7,
+      child: FutureBuilder(
+          future: fetchBoardingPassDetails(flightNo, pnr),
+          builder: (context, AsyncSnapshot<Map> snapshot) {
+            if (snapshot.hasData) {
+              return Column(
+                children: [
+                  Text(
+                    snapshot.data!['message']['name'],
+                    style: TextStyle(fontSize: 27, letterSpacing: 1.1),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Flight No.',
+                          style: TextStyle(fontSize: 17, letterSpacing: 1.1),
                         ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          data['message']['name'],
-                          style: const TextStyle(
-                              color: Colors.grey,
-                              fontSize: 40,
-                              fontWeight: FontWeight.w500,
-                              letterSpacing: 1.1),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          data['message']['id'],
-                          style: TextStyle(
-                              color: Colors.grey[400],
-                              fontSize: 17,
-                              letterSpacing: 1.1),
-                        ),
+                        Text(flightNo),
                       ],
                     ),
-                  )),
-            ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Seat',
+                          style: TextStyle(fontSize: 17, letterSpacing: 1.1),
+                        ),
+                        Text(snapshot.data!['message']['seat']),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10.0, vertical: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Class',
+                          style: TextStyle(fontSize: 17, letterSpacing: 1.1),
+                        ),
+                        Text(snapshot.data!['message']['class']),
+                      ],
+                    ),
+                  )
+                ],
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          }),
+    );
+  }
+}
+
+class PassForm extends StatelessWidget {
+  const PassForm(
+      {Key? key,
+      required this.flightNoController,
+      required this.pnrController,
+      required this.submitHandler})
+      : super(key: key);
+  final TextEditingController flightNoController;
+  final TextEditingController pnrController;
+  final Function submitHandler;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(bottom: 25),
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.all(Radius.circular(15))),
+          width: MediaQuery.of(context).size.width * 0.6,
+          child: TextField(
+            controller: flightNoController,
+            decoration: const InputDecoration(
+                hintText: 'Enter Flight Number', border: InputBorder.none),
           ),
         ),
-        const SizedBox(
-          height: 40,
+        Container(
+          margin: const EdgeInsets.only(bottom: 25),
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.all(Radius.circular(15))),
+          width: MediaQuery.of(context).size.width * 0.6,
+          child: TextField(
+            controller: pnrController,
+            decoration: const InputDecoration(
+                hintText: 'Enter PNR Number', border: InputBorder.none),
+          ),
         ),
-        BialRewards(
-          points: data['message']['rewards'],
-        ),
-        const Options(
-            label: 'My Orders',
-            iconData: Icons.shopping_bag_rounded,
-            navigateTo: MyOrders())
+        ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.indigo[300])),
+            onPressed: () {
+              submitHandler();
+            },
+            child: Text('Submit'))
       ],
     );
   }
 }
 
 class Options extends StatelessWidget {
-  const Options(
-      {Key? key,
-      required this.label,
-      required this.iconData,
-      required this.navigateTo})
-      : super(key: key);
+  const Options({
+    Key? key,
+    required this.label,
+    required this.iconData,
+    required this.onTapHandler,
+  }) : super(key: key);
 
   final String label;
   final IconData iconData;
-  final Widget navigateTo;
+  final void Function() onTapHandler;
 
   @override
   Widget build(BuildContext context) {
@@ -210,9 +421,6 @@ class Options extends StatelessWidget {
             ],
           ),
         ),
-        onTap: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => navigateTo));
-        });
+        onTap: onTapHandler);
   }
 }
